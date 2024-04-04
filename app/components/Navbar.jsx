102d775e3b06fcf0b4c1bc19bbeda58b57a01dbe
Fuku <53621037+FukurouHaiRiyo@users.noreply.github.com>
2024-04-04 17:59:54 +0300

    updates

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
             <button type='button' className='-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700'>
                 <span className='sr-only'></span>
 
-                <svg className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' aria-hidden='true'>
+                <svg className='h-6 w-6' fill='none' viewBox='0 0 24 24' strokeWidth='1.5' stroke='currentColor' aria-hidden='true'>
                     <path strokeLinecap='round' strokeLinejoin='round' d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'/>
                 </svg>
             </button>
